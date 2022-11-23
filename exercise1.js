fb9b193d5ed692594cec624505414ef78f43e35f
Mateusz Lewandowski <mateusz.lewandowski90@gmail.com>
2022-11-23 14:58:00 +0100

    excercise 1.3 - task

--- exercise1.js
+++ exercise1.js
@@ -21,27 +21,20 @@ function login(login, password) {
     return false;
   }
 
-  const amountOfUsers = Object.keys(users).length;
   return {
     role: user.role,
     name: user.name,
     surname: user.surname,
-    countOtherUsers: function countOtherUsers() {
-      return amountOfUsers;
-    },
   };
 }
 
-const userData = login("admin", "123"); // {role: string, name: string, surname: string, countOtherUsers: () => number}
-userData.countOtherUsers(); // -> 2 it is getting it from users map, and now it's not any more
+const userData = login("admin", "123");
+// pseudocode:
+register({
+  name: "Mateusz",
+  surname: "Lewandowski",
+  login: "mat",
+  pass: "test",
+});
 
-userData.displayAvatar();
-// userData.pass = "other";
-const userData2 = login("admin", "123");
-
-console.log({ userData, userData2, same: userData === userData2 }); //? 2 for false
-
-// console.log("2", login("admin", "somethingDifferent")); //? 0 for false
-
-// console.log("user1", login("user1", "123")); // ->
-// console.log("admin", login("admin", "123")); // -> some possibility to check if this user is an admin, his/her name and surname
+login("mat", "test"); // -> {name: 'Mateusz', surname: 'Lewandowski', role: 'user'}
