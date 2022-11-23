483c7c7ba1157a42c53c6337cca617bbe963f8b0
Mateusz Lewandowski <mateusz.lewandowski90@gmail.com>
2022-11-23 14:03:13 +0100

    excersise 1.2

--- exercise1.js
+++ exercise1.js
@@ -10,6 +10,7 @@ function login(login, password) {
       role: "admin",
     },
   };
+
   const user = users[login];
 
   if (!user) {
@@ -20,12 +21,40 @@ function login(login, password) {
     return false;
   }
 
+  // return user;
+  const amountOfUsers = Object.keys(users).length;
   return {
     role: user.role,
     name: user.name,
     surname: user.surname,
+    countOtherUsers: function countOtherUsers() {
+      return amountOfUsers;
+    },
   };
 }
+
+const userData = login("admin", "123"); // {role: string, name: string, surname: string, countOtherUsers: () => number}
+userData.countOtherUsers(); // -> 2 it is getting it from users map, and now it's not any more
+
+userData.displayAvatar();
+// userData.pass = "other";
+const userData2 = login("admin", "123");
+
+console.log({ userData, userData2, same: userData === userData2 }); //? 2 for false
+
+const objectA = { a: 1 };
+const objectB = { a: 1 };
+const objectC = objectA;
+console.log({
+  objectA,
+  objectB,
+  objectC,
+  "A=B": objectA === objectB,
+  "A=C": objectA === objectC,
+  "B=C": objectB === objectC,
+});
+
+// console.log("2", login("admin", "somethingDifferent")); //? 0 for false
 
 // console.log("user1", login("user1", "123")); // ->
 // console.log("admin", login("admin", "123")); // -> some possibility to check if this user is an admin, his/her name and surname
