9d16d17443a89cf7647348c5e29367c5d3b2d3ac
Mateusz Lewandowski <mateusz.lewandowski90@gmail.com>
2022-11-24 12:51:38 +0100

    functions

---
+++ function-2.js
@@ -0,0 +1,14 @@
+var someValue;
+function test() {
+  console.log("some actions");
+}
+
+console.log("do something....");
+
+someValue = 2;
+let someValue2;
+someValue2 = 2;
+
+const someValue3 = 2;
+
+test();
--- functions.js
+++ functions.js
@@ -1,12 +1,50 @@
-const someModule = (function () {
-  const variable = "something";
-  // do some logic, define some functions...
+const someFunctionWithArgs = function (
+  firstArgument,
+  secondArgument,
+  thirdArgument,
+  fourthArgument
+) {
+  const test = arguments[4]; //?
+  someObject === secondArgument; // -> true
+  return "someValue";
+};
 
-  function doSomeWork() {}
+const test = 4;
+const someObject = { a: 1 }; // { a: 1 } - object in the memory at address 1 (ref1)
 
-  return {
-    doSomeWork: doSomeWork,
-  };
-})();
+var v = 5;
+someFunction(test, someObject, 34, { a: 2 }, "test"); //?
+// someFunctionWithArgs(test, someObject, 34, { a: 2 }, "test"); //?
 
-doSomeWork(); // -> error
+// const someOtherFunction = function () {
+//   return "some value";
+// };
+function someFunction() {
+  var firstArgument = arguments[0]; //?
+  var secondArgument = arguments[1]; //?
+  var fourthArgument = arguments[3]; //?
+  var thirdArgument = arguments[2]; //?
+  console.log(firstArgument + fourthArgument);
+
+  someObject === secondArgument; // -> true
+  return "someValue";
+}
+
+// const anything = someFunction;
+
+// someFunction(someFunctionWithArgs);
+
+// someFunction["newProperty"] = "someValue";
+
+// console.log(test); //? 4
+
+// const someModule = (function () {
+//   const variable = "something";
+//   // do some logic, define some functions...
+
+//   function doSomeWork() {}
+
+//   return {
+//     doSomeWork: doSomeWork,
+//   };
+// })();
