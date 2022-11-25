9f3a6001fcab08052088527d8fd94fd7065a760c
Mateusz Lewandowski <mateusz.lewandowski90@gmail.com>
2022-11-25 12:33:32 +0100

    arrow functions

---
+++ function-3.js
@@ -0,0 +1,62 @@
+function functionDeclaration(arg1) {
+  // hoisted
+  // this comes from context
+  console.log(arg1);
+}
+
+const functionExpression = function (arg1) {
+  // not hoisted
+  // this comes from context
+  console.log(arg1);
+};
+
+const arrowFunction = (arg1) => {
+  // not hoisted
+  // this comes from closure
+  return `Hello ${arg1}`;
+};
+//prettier-ignore
+const simpleArrowFunction = arg1 => `Hello ${arg1}`
+
+function saveDataToDatabase(data, done) {
+  //doing some stuff to save data
+  done();
+}
+
+class User {
+  #saved;
+  constructor(name) {
+    this.name = name;
+    this.#saved = false;
+  }
+
+  saveUserToDatabase() {
+    saveDataToDatabase(
+      {
+        id: "generatedId",
+        name: this.name,
+      },
+      function () {
+        this.#saved = true;
+      }.bind(this)
+    );
+  }
+  saveUserToDatabaseWitArrowFunction() {
+    saveDataToDatabase(
+      {
+        id: "generatedId",
+        name: this.name,
+      },
+      () => {
+        this.#saved = true;
+      }
+    );
+  }
+  isSaved() {
+    return this.#saved;
+  }
+}
+
+const user = new User("Mat");
+user.saveUserToDatabaseWitArrowFunction();
+user.isSaved(); //?
